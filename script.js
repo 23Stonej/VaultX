df92d36c86c95cf655132d9ab84db023ece52377
23Stonej <jackstonemoney7@gmail.com>
2026-02-24 22:06:23 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ function enterDashboard() {
 function goHome() {
   window.location.href = "index.html";
 }
+
+document.addEventListener("mousemove", function(e) {
+  const card = document.querySelector(".card");
+  if (!card) return;
+
+  const x = (window.innerWidth / 2 - e.pageX) / 25;
+  const y = (window.innerHeight / 2 - e.pageY) / 25;
+
+  card.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+});

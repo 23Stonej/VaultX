9d9e81975a05dc8de0d470dcc0372ee0c4dc3106
23Stonej <jackstonemoney7@gmail.com>
2026-02-25 21:21:22 +0000

    Update login.js

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -1,1 +1,28 @@
+let users = [];
 
+exports.handler = async (event) => {
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: "Method Not Allowed"
+    };
+  }
+
+  const { email, password } = JSON.parse(event.body);
+
+  const user = users.find(
+    user => user.email === email && user.password === password
+  );
+
+  if (!user) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ error: "Invalid credentials" })
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Login successful" })
+  };
+};

cd7f47b2331fefb1bdc284472a1be8d1dc32a9b0
23Stonej <jackstonemoney7@gmail.com>
2026-02-25 22:06:57 +0000

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,24 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD03N3_jRsw0l4a56WH6F75Zj0_zHipkIo",
-  authDomain: "vaultx-43488.firebaseapp.com",
-  projectId: "vaultx-43488",
-  storageBucket: "vaultx-43488.firebasestorage.app",
-  messagingSenderId: "103145535155",
-  appId: "1:103145535155:web:7afce57dac2c968c2122c6",
-  measurementId: "G-4WSJNZSQJ8"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Paste your Firebase config here
 const firebaseConfig = {
   apiKey: "AIzaSyD03N3_jRsw0l4a56WH6F75Zj0_zHipkIo",
   authDomain: "vaultx-43488.firebaseapp.com",
@@ -31,5 +11,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
+
+// Make auth and db global for other scripts
+var auth = firebase.auth();
+var db = firebase.firestore();

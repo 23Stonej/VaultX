2247c49a080a196a4acf5d9b89d585d86a81e843
23Stonej <jackstonemoney7@gmail.com>
2026-02-26 19:48:46 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // 🔥 YOUR FIREBASE CONFIG (CHECK THIS CAREFULLY)
 const firebaseConfig = {
-  apiKey: "PASTE_YOURS_HERE",
-  authDomain: "PASTE_YOURS_HERE",
-  projectId: "PASTE_YOURS_HERE",
-  storageBucket: "PASTE_YOURS_HERE",
-  messagingSenderId: "PASTE_YOURS_HERE",
-  appId: "PASTE_YOURS_HERE"
+ apiKey: "AIzaSyD03N3_jRsw0l4a56WH6F75Zj0_zHipkIo",
+  authDomain: "vaultx-43488.firebaseapp.com",
+  projectId: "vaultx-43488",
+  storageBucket: "vaultx-43488.firebasestorage.app",
+  messagingSenderId: "103145535155",
+  appId: "1:103145535155:web:7afce57dac2c968c2122c6",
+  measurementId: "G-4WSJNZSQJ8"
 };
 
 // Initialize Firebase
